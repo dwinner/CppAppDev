07034fc480e137da32f11b48836d363664a95e31
jdeveloper85 <dwinner@inbox.ru>
2016-04-09 10:42:43 +0300

    C++ AppDev - Adding the vector sample

--- Introduction/VectorSample/VectorSample.cpp
+++ Introduction/VectorSample/VectorSample.cpp
@@ -1,23 +1,32 @@
-// vectortest.cpp
+// VectorSample.cpp: Использование STL-контейнеров.
 
+#include "stdafx.h"
 #include <string>
 #include <vector>
 #include <iostream>
 
 using namespace std;
 
-int main() 
+int main()
 {
-	// Create a vector of strings, using C++11 uniform initialization
-	vector<string> myVector = {"A first string", "A second string"};
-	// Add some strings to the vector using push_back
+	// Создать вектор из строк, используя синтаксис инициализации C++11
+	vector<string> myVector = { "a first string","A second string" };
+
+	// Добавить некоторые строки в вектор
 	myVector.push_back("A third string");
 	myVector.push_back("The last string in the vector");
-	// Iterate over the elements in the vector and print them
+
+	// Итерирование по вектору
 	for (auto iterator = myVector.cbegin(); iterator != myVector.cend(); ++iterator)
+	{
 		cout << *iterator << endl;
-	// Print the elements again using C++11 range-based for loop
+	}
+
 	for (auto& str : myVector)
+	{
 		cout << str << endl;
+	}
+
 	return 0;
 }
+
