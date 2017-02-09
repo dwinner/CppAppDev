a662b845643a1e55ae7d040fa00c02bef8947e09
bboytronik@gmail.com <bboytronik@gmail.com>
2017-02-09 22:53:39 +0300

    Cell assigner

---
+++ Cpp.Creation/AssignerSample/SpreadsheetCell.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include <string>
+using std::string;
+
+class SpreadsheetCell
+{
+public:
+	SpreadsheetCell();
+	explicit SpreadsheetCell(double initialValue);
+	explicit SpreadsheetCell(const string& initialValue);
+	SpreadsheetCell(const SpreadsheetCell& rhs);
+	SpreadsheetCell& operator=(const SpreadsheetCell& rhs);
+	void setValue(double inValue);
+	double getValue() const;
+	void setString(const string& inString);
+	string getString() const;
+
+protected:
+	static string doubleToString(double inValue);
+	static double stringToDouble(const string& inString);
+
+	double mValue;
+	string mString;
+};
