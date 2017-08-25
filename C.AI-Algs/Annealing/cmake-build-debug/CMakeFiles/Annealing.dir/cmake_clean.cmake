71d0b8d49638b6df46c8f20102f86677f516665c
DeVi <dwinner@inbox.ru>
2017-08-25 17:57:18 +0300

    Adapting AI-projects for CLion C++ toolchain

---
+++ C.AI-Algs/Annealing/cmake-build-debug/CMakeFiles/Annealing.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Annealing.dir/entryPoint.c.obj"
+  "CMakeFiles/Annealing.dir/helpers.c.obj"
+  "Annealing.pdb"
+  "Annealing.exe"
+  "Annealing.exe.manifest"
+  "libAnnealing.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Annealing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- C.AI-Algs/Annealing/entryPoint.c
+++ C.AI-Algs/Annealing/entryPoint.c
@@ -1,8 +1,11 @@
+#include <stdio.h>
+
 /**
  *	Annealing alg
  */
 
 int main(int argc, char* argv[])
 {
+   printf("Ok");
    return 0;
 }
