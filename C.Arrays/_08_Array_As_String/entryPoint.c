259fb7cfd7793bbe2e84e84b716bed9b5583ae06
DeVi <dwinner@inbox.ru>
2017-11-30 14:47:32 +0300

    C style arrays / WIP

---
+++ C.Arrays/_08_Array_As_String/entryPoint.c
@@ -0,0 +1,28 @@
+﻿/**
+ * Treating character arrays as strings
+ */
+#include <stdio.h>
+#define SIZE 20
+
+int main(void)
+{
+   char string1[SIZE];
+   char string2[] = "string literal";
+
+   // read string from user into array string1
+   printf("%s", "Enter a string (no longer than 19 characters): ");
+   scanf_s("%19s", string1);
+
+   // output strings
+   printf("string1 is: %s\nstring2 is: %s\nstring1 with spaces between characters is:\n",
+          string1,
+          string2);
+
+   // output characters until null character is reached  
+   for (size_t i = 0; i < SIZE && string1[i] != '\0'; ++i)
+   {
+      printf("%c ", string1[i]);
+   }
+
+   return 0;
+}
---
+++ C.Arrays/_09_Static_Array/entryPoint.c
@@ -0,0 +1,68 @@
+﻿/**
+ * Static arrays are initialized to zero if not explicitly initialized
+ */
+#include <stdio.h>
+
+void staticArrayInit(void);
+void automaticArrayInit(void);
+
+int main(void)
+{
+   puts("First call to each function:");
+   staticArrayInit();
+   automaticArrayInit();
+
+   puts("\n\nSecond call to each function:");
+   staticArrayInit();
+   automaticArrayInit();
+
+   return 0;
+}
+
+/**
+ * \brief Function to demonstrate an automatic local array
+ */
+void automaticArrayInit(void)
+{
+   int array2[3] = {1, 2, 3};
+
+   puts("\n\nValues on entering automatic ArrayInit:");
+
+   for (size_t i = 0; i <= 2; ++i)
+   {
+      printf("array2[%u] = %d  ", i, array2[i]);
+   }
+
+   puts("\nValues on exiting automaticArrayInit:");
+
+   // modify and output contents of array2
+   for (size_t i = 0; i <= 2; ++i)
+   {
+      printf("array2[%u] = %d  ", i, array2[i] += 5);
+   }
+}
+
+/**
+ * \brief Function to demonstrate a static local array
+ */
+void staticArrayInit(void)
+{
+   // initializes elements to 0 before the function is called
+   static int array1[3];
+
+   puts("\nValues on entering staticArrayInit:");
+
+   // output contents of array1
+   for (size_t i = 0; i <= 2; ++i)
+   {
+      printf("array1[%u] = %d  ", i, array1[i]);
+   }
+
+   puts("\nValues on exiting staticArrayInit:");
+
+   // modify and output contents of array1
+   for (size_t i = 0; i <= 2; ++i)
+   {
+      printf("array1[%u] = %d  ", i, array1[i] += 5);
+   }
+}
