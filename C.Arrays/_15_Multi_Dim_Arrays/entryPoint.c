09c8aaff6befee6a3235e90a347af090497e187c
DeVi <dwinner@inbox.ru>
2017-12-26 17:54:09 +0300

    C style arrays / completed

---
+++ C.Arrays/_15_Multi_Dim_Arrays/entryPoint.c
@@ -0,0 +1,42 @@
+﻿/**
+ * Initializing multidimensional arrays
+ */
+#include <stdio.h>
+
+void printArray(const int a[][3]);
+
+int main(void)
+{
+   int array1[2][3] = {{1, 2, 3}, {4, 5, 6}};
+   puts("Values in array1 by row are:");
+   printArray(array1);
+
+   int array2[2][3] = {1, 2, 3, 4, 5};
+   puts("Values in array2 by row are:");
+   printArray(array2);
+
+   int array3[2][3] = {{1, 2}, {4}};
+   puts("Values in array3 by row are:");
+   printArray(array3);
+
+   return 0;
+}
+
+/**
+ * Output the array
+ * @param a Array
+ */
+void printArray(const int a[][3])
+{
+   // loop through rows
+   for (size_t i = 0; i <= 1; ++i)
+   {
+      // output column values
+      for (size_t j = 0; j <= 2; ++j)
+      {
+         printf("%d ", a[i][j]);
+      }
+
+      printf("\n");
+   }
+}
--- C.Arrays/_16_TwoDim_Array_Sample/entryPoint.c
+++ C.Arrays/_16_TwoDim_Array_Sample/entryPoint.c
@@ -1,5 +1,7 @@
-// Fig. 6.22: fig06_22.c
-// Two-dimensional array manipulations.
+﻿/**
+ * Two-dimensional array manipulations.
+ */
+
 #include <stdio.h>
 #define STUDENTS 3
 #define EXAMS 4
@@ -14,10 +16,12 @@ void printArray(const int grades[][EXAMS], size_t pupils, size_t tests);
 int main(void)
 {
    // initialize student grades for three students (rows)
-   int studentGrades[STUDENTS][EXAMS] =  
-      { { 77, 68, 86, 73 },
-        { 96, 87, 89, 78 },
-        { 70, 90, 86, 81 } };
+   int studentGrades[STUDENTS][EXAMS] =
+   {
+      {77, 68, 86, 73},
+      {96, 87, 89, 78},
+      {70, 90, 86, 81}
+   };
 
    // output array studentGrades
    puts("The array is:");
@@ -25,84 +29,91 @@ int main(void)
 
    // determine smallest and largest grade values
    printf("\n\nLowest grade: %d\nHighest grade: %d\n",
-      minimum(studentGrades, STUDENTS, EXAMS),
-      maximum(studentGrades, STUDENTS, EXAMS));
+          minimum(studentGrades, STUDENTS, EXAMS),
+          maximum(studentGrades, STUDENTS, EXAMS));
 
    // calculate average grade for each student
-   for (size_t student = 0; student < STUDENTS; ++student) {
-      printf("The average grade for student %u is %.2f\n", 
-         student, average(studentGrades[student], EXAMS));
-   } 
+   for (size_t student = 0; student < STUDENTS; ++student)
+   {
+      printf("The average grade for student %u is %.2f\n",
+             student, average(studentGrades[student], EXAMS));
+   }
+
+   return 0;
 }
 
 // Find the minimum grade
-int minimum(const int grades[][EXAMS], size_t pupils, size_t tests)
+int minimum(const int grades[][EXAMS], const size_t pupils, const size_t tests)
 {
    int lowGrade = 100; // initialize to highest possible grade
 
    // loop through rows of grades
-   for (size_t i = 0; i < pupils; ++i) {
-
+   for (size_t i = 0; i < pupils; ++i)
+   {
       // loop through columns of grades
-      for (size_t j = 0; j < tests; ++j) {
-
-         if (grades[i][j] < lowGrade) {
+      for (size_t j = 0; j < tests; ++j)
+      {
+         if (grades[i][j] < lowGrade)
+         {
             lowGrade = grades[i][j];
-         } 
-      } 
-   } 
+         }
+      }
+   }
 
    return lowGrade; // return minimum grade 
-} 
+}
 
 // Find the maximum grade
-int maximum(const int grades[][EXAMS], size_t pupils, size_t tests)
+int maximum(const int grades[][EXAMS], const size_t pupils, const size_t tests)
 {
    int highGrade = 0; // initialize to lowest possible grade
 
    // loop through rows of grades
-   for (size_t i = 0; i < pupils; ++i) {
-
+   for (size_t i = 0; i < pupils; ++i)
+   {
       // loop through columns of grades
-      for (size_t j = 0; j < tests; ++j) {
-
-         if (grades[i][j] > highGrade) {
+      for (size_t j = 0; j < tests; ++j)
+      {
+         if (grades[i][j] > highGrade)
+         {
             highGrade = grades[i][j];
-         } 
-      } 
-   } 
+         }
+      }
+   }
 
    return highGrade; // return maximum grade
-} 
+}
 
 // Determine the average grade for a particular student
-double average(const int setOfGrades[], size_t tests)      
-{                                                         
+double average(const int setOfGrades[], const size_t tests)
+{
    int total = 0; // sum of test grades                
-                                                          
+
    // total all grades for one student                 
-   for (size_t i = 0; i < tests; ++i) {                        
-      total += setOfGrades[i];                          
-   }                                         
-                                                          
-   return (double) total / tests; // average         
-} 
+   for (size_t i = 0; i < tests; ++i)
+   {
+      total += setOfGrades[i];
+   }
+
+   return (double)total / tests; // average         
+}
 
 // Print the array
-void printArray(const int grades[][EXAMS], size_t pupils, size_t tests)
+void printArray(const int grades[][EXAMS], const size_t pupils, const size_t tests)
 {
    // output column heads
    printf("%s", "                 [0]  [1]  [2]  [3]");
 
    // output grades in tabular format
-   for (size_t i = 0; i < pupils; ++i) {
-
+   for (size_t i = 0; i < pupils; ++i)
+   {
       // output label for row
       printf("\nstudentGrades[%u] ", i);
 
       // output grades for one student
-      for (size_t j = 0; j < tests; ++j) {
+      for (size_t j = 0; j < tests; ++j)
+      {
          printf("%-5d", grades[i][j]);
-      } 
-   } 
+      }
+   }
 }
