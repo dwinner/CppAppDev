466043f005c7d399c78d73fa39fae8dd5b1dc0e6
DeVi <dwinner@inbox.ru>
2018-01-17 12:47:34 +0300

    Pointers in C / WIP

---
+++ C.Pointers/Size_Of/size_of.c
@@ -0,0 +1,24 @@
+﻿/**
+ * Applying sizeof to an array name returns
+ * the number of bytes in the array
+ */
+#include <stdio.h>
+
+#define SIZE 20
+
+size_t getSize(const float* ptr);
+
+int main()
+{
+   float array[SIZE]; // create array
+   printf("The number of bytes in the array is %u"
+          "\nThe number of bytes returned by getSize is %u\n",
+          sizeof array, getSize(array));
+
+   return 0;
+}
+
+size_t getSize(const float* ptr)
+{
+   return sizeof ptr;
+}
---
+++ C.Pointers/Uppercase_Via_Ptr/entry_pont.c
@@ -0,0 +1,29 @@
+﻿/**
+ * Converting a string to uppercase using a
+ * non-constant pointer to non-constant data.
+ */
+
+#include <stdio.h>
+#include <ctype.h>
+
+void convertToUppercase(char* sPtr);
+
+int main()
+{
+   char string[] = "cHaRaCters and $32.98"; // initialize char array
+
+   printf("The string before conversion is: %s", string);
+   convertToUppercase(string);
+   printf("\nThe string after conversion is: %s\n", string);
+
+   return 0;
+}
+
+void convertToUppercase(char* sPtr)
+{
+   while (*sPtr != '\0')
+   {
+      *sPtr = toupper(*sPtr);
+      ++sPtr;
+   }
+}
