3fd7beba38d77a299e955d556802f34d561efc09
DeVi <dwinner@inbox.ru>
2018-02-22 09:24:07 +0300

    C strings / wip

--- @ToCode/C.Strings.ToCode/fig08_04.c
+++ @ToCode/C.Strings.ToCode/fig08_04.c
@@ -40,20 +40,3 @@ int main(void)
        "Space", isgraph(' ') ? " is a " : " is not a ",
        "printing character other than a space");
 } 
-
-
-
-/**************************************************************************
- * (C) Copyright 1992-2015 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
--- @ToCode/C.Strings.ToCode/fig08_10.c
+++ @ToCode/C.Strings.ToCode/fig08_10.c
@@ -30,20 +30,3 @@ void reverse(const char * const sPtr)
       putchar(sPtr[0]); // use putchar to display character
    } 
 }
-
-
-
-/**************************************************************************
- * (C) Copyright 1992-2015 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
