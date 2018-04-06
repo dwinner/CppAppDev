a29ecd0fcbd3b189864dcc5af9bb4cc1a8d7d40e
DeVi <dwinner@inbox.ru>
2018-04-06 16:54:50 +0300

    Bitwise operations / WIP

--- C.BitOperations/2-Bitwise_Operations/bitwise_ops.c
+++ C.BitOperations/2-Bitwise_Operations/bitwise_ops.c
@@ -1,15 +1,16 @@
-// Fig. 10.9: fig10_09.c
-// Using the bitwise AND, bitwise inclusive OR, bitwise
-// exclusive OR and bitwise complement operators
+﻿/**
+ * Using the bitwise AND, bitwise inclusive OR, bitwise
+ * exclusive OR and bitwise complement operators
+ */
 #include <stdio.h>
 
-void displayBits(unsigned int value); // prototype
+void displayBits(unsigned int value);
 
 int main(void)
-{ 
+{
    // demonstrate bitwise AND (&)
    unsigned int number1 = 65535;
-   unsigned int mask = 1;
+   const unsigned int mask = 1;
    puts("The result of combining the following");
    displayBits(number1);
    displayBits(mask);
@@ -18,7 +19,7 @@ int main(void)
 
    // demonstrate bitwise inclusive OR (|)
    number1 = 15;
-   unsigned int setBits = 241;
+   const unsigned int setBits = 241;
    puts("\nThe result of combining the following");
    displayBits(number1);
    displayBits(setBits);
@@ -27,7 +28,7 @@ int main(void)
 
    // demonstrate bitwise exclusive OR (^)
    number1 = 139;
-   unsigned int number2 = 199;
+   const unsigned int number2 = 199;
    puts("\nThe result of combining the following");
    displayBits(number1);
    displayBits(number2);
@@ -40,25 +41,33 @@ int main(void)
    displayBits(number1);
    puts("is");
    displayBits(~number1);
-} 
 
-// display bits of an unsigned int value
+   return 0;
+}
+
+/**
+ * \brief Display bits of an unsigned int value
+ * \param value Value
+ */
 void displayBits(unsigned int value)
-{ 
+{
    // declare displayMask and left shift 31 bits
-   unsigned int displayMask = 1 << 31;
+   const unsigned int displayMask = 1 << 31;
 
    printf("%10u = ", value);
 
    // loop through bits
-   for (unsigned int c = 1; c <= 32; ++c) { 
+   for (unsigned int c = 1; c <= 32; ++c)
+   {
       putchar(value & displayMask ? '1' : '0');
       value <<= 1; // shift value left by 1
 
-      if (c % 8 == 0) { // output a space after 8 bits
+      if (c % 8 == 0)
+      {
+         // output a space after 8 bits
          putchar(' ');
-      } 
-   } 
+      }
+   }
 
    putchar('\n');
 }
