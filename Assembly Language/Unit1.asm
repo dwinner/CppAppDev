9e94b309d8a8ad64606f2228ca323ce800322ee9
DeVi <dwinner@inbox.ru>
2018-04-13 16:55:47 +0300

    ASM

---
+++ Assembly Language/Unit1.asm
@@ -0,0 +1,27 @@
+.386
+.model flat, stdcall
+option casemap:none
+
+;include modules=======================================
+;include %Paste MASM Directory here!%\include\windows.inc
+;include %Paste MASM Directory here!%\include\user32.inc
+;include %Paste MASM Directory here!%\include\kernel32.inc
+;======================================================
+
+;librarys==============================================
+;includelib {Paste MASM Directory here!}\lib\user32.lib
+;includelib {Paste MASM Directory here!}\lib\kernel32.lib
+;======================================================
+
+;segments =============================================
+
+;place your data here
+.data
+
+;place your executable code here
+.code
+
+start: ;- entry point
+
+end start
+;======================================================
