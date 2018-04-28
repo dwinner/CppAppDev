350d33a73a8bef94675f1523a46d740961f7f076
DeVi <dwinner@inbox.ru>
2018-04-28 16:27:47 +0300

    ADT in C / Simple queue impl

---
+++ C.ADT/QueueImpls/SimpleQueue/main.c
@@ -0,0 +1,192 @@
+﻿/**
+ * Operating and maintaining a queue
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/**
+ * \brief Self-referential structure
+ */
+struct queueNode
+{
+   /**
+    * \brief Define data as a char
+    */
+   char data;
+
+   /**
+    * \brief queueNode pointer
+    */
+   struct queueNode* nextPtr;
+};
+
+typedef struct queueNode QueueNode;
+typedef QueueNode* QueueNodePtr;
+
+/* Function prototypes */
+
+void printQueue(QueueNodePtr currentPtr);
+bool isEmpty(QueueNodePtr headPtr);
+char dequeue(QueueNodePtr* headPtr, QueueNodePtr* tailPtr);
+bool enqueue(QueueNodePtr* headPtr, QueueNodePtr* tailPtr, char value);
+void instructions();
+
+int main(void)
+{
+   QueueNodePtr headPtr = NULL; // initialize headPtr
+   QueueNodePtr tailPtr = NULL; // initialize tailPtr
+   char item; // char input by user
+
+   instructions(); // display the menu
+   printf("%s", "? ");
+   unsigned int choice; // user's menu choice
+   scanf_s("%u", &choice);
+
+   // while user does not enter 3
+   while (choice != 3)
+   {
+      switch (choice)
+      {
+      case 1: // enqueue value
+         printf("%s", "Enter a character: ");
+         scanf_s("%c\n", &item); // BUG: In scanning characters
+         if (enqueue(&headPtr, &tailPtr, item))
+         {
+            printQueue(headPtr);
+         }
+         else
+         {
+            printf("%c not inserted. No memory available.\n", item);
+         }
+
+         break;
+
+      case 2: // dequeue value
+         // if queue is not empty
+         if (!isEmpty(headPtr))
+         {
+            item = dequeue(&headPtr, &tailPtr);
+            printf("%c has been dequeued.\n", item);
+         }
+
+         printQueue(headPtr);
+         break;
+
+      default:
+         puts("Invalid choice.\n");
+         instructions();
+         break;
+      }
+
+      printf("%s", "? ");
+      scanf_s("%u", &choice);
+   }
+
+   puts("End of run.");
+
+   return 0;
+}
+
+/**
+ * \brief Display program instructions to user
+ */
+void instructions()
+{
+   printf("Enter your choice:\n"
+      "   1 to add an item to the queue\n"
+      "   2 to remove an item from the queue\n"
+      "   3 to end\n");
+}
+
+/**
+ * \brief Insert a node at queue tail
+ * \param headPtr - Head of the queue
+ * \param tailPtr - Tail of the queue
+ * \param value - value to enqueue
+ */
+bool enqueue(QueueNodePtr* headPtr, QueueNodePtr* tailPtr, const char value)
+{
+   QueueNodePtr newPtr = malloc(sizeof(QueueNode));
+
+   if (newPtr != NULL) // is space available
+   {
+      newPtr->data = value;
+      newPtr->nextPtr = NULL;
+
+      // if empty, insert node at head
+      if (isEmpty(*headPtr))
+      {
+         *headPtr = newPtr;
+      }
+      else
+      {
+         (*tailPtr)->nextPtr = newPtr;
+      }
+
+      *tailPtr = newPtr;
+
+      return true;
+   }
+
+   return false;
+}
+
+/**
+ * \brief Remove node from the queue head
+ * \param headPtr - Head of the queue
+ * \param tailPtr - Tail of the queue
+ * \return Dequeued node
+ */
+char dequeue(QueueNodePtr* headPtr, QueueNodePtr* tailPtr)
+{
+   const char value = (*headPtr)->data;
+   QueueNodePtr tempPtr = *headPtr;
+   *headPtr = (*headPtr)->nextPtr;
+
+   // if queue is empty
+   if (*headPtr == NULL)
+   {
+      *tailPtr = NULL;
+   }
+
+   free(tempPtr);
+   return value;
+}
+
+/**
+ * \brief Finds out whether the queue is empty or not
+ * \param headPtr The head of the queue
+ * \return true, if queue is empty, false - otherwise
+ */
+bool isEmpty(QueueNodePtr headPtr)
+{
+   return headPtr == NULL;
+}
+
+/**
+ * \brief Print the queue
+ * \param currentPtr Starting pointer
+ */
+void printQueue(QueueNodePtr currentPtr)
+{
+   // if queue is empty
+   if (currentPtr == NULL)
+   {
+      puts("Queue is empty.\n");
+   }
+   else
+   {
+      puts("The queue is:");
+
+      // while not end of queue
+      while (currentPtr != NULL)
+      {
+         printf("%c --> ", currentPtr->data);
+         currentPtr = currentPtr->nextPtr;
+      }
+
+      puts("NULL\n");
+   }
+}
--- delete_unvcs.bat
+++ delete_unvcs.bat
@@ -6,6 +6,7 @@ for /d /r "." %%d in (Debug) do @if exist "%%d" rd /s/q "%%d"
 for /d /r "." %%d in (Release) do @if exist "%%d" rd /s/q "%%d"
 for /d /r "." %%d in (TestResults) do @if exist "%%d" rd /s/q "%%d"
 for /d /r "." %%d in (cmake-build-debug) do @if exist "%%d" rd /s/q "%%d"
+for /d /r "." %%d in (ipch) do @if exist "%%d" rd /s/q "%%d"
 del /f /s /q .\*.sdf
 del /f /s /q .\*.user
 pause
