690aeaaae2301c1d5bda5eb1a8aed304f3f81ed4
Denis Vinevcev <dwinner@inbox.ru>
2019-08-21 21:44:05 +0300

    Improved evaluation parser / in progress

--- Apps/Syntax parsing, Evaluation/Simple parser (improved)/eval_parser.c
+++ Apps/Syntax parsing, Evaluation/Simple parser (improved)/eval_parser.c
@@ -4,13 +4,13 @@
 #include <string.h>
 #include "eval_parser.h"
 
-void start_evaluation(double *answer)
+void start_evaluation(double* answer)
 {
    get_token();
-   
+
    if (!*token)
    {
-      set_error(2);  /* TODO: Introduce enums for error codes */
+      set_error(2); /* TODO: Introduce enums for error codes */
       return;
    }
 
@@ -19,61 +19,180 @@ void start_evaluation(double *answer)
    if (*token)
    {
       /* last token must be null */
-      set_error(0);  /* TODO: Introduce enums for error codes */
+      set_error(0); /* TODO: Introduce enums for error codes */
    }
 }
 
-void eval_exp1(double *answer)
+void eval_exp1(double* answer)
 {
+   int slot;
+   char ttok_type;
+   char temp_token[PROGRAM_BUFFER];
 
+   if (token_type == VARIABLE)
+   {
+      /* save old token */
+      strcpy_s(temp_token, PROGRAM_BUFFER, token);
+      ttok_type = token_type;
+
+      /* compute the index of the variable */
+      slot = toupper(*token) - 'A';
+
+      get_token();
+
+      if (*token != '=')
+      {
+         put_back(); /* return current token */
+
+         /* restore old token - not assignment */
+         strcpy_s(token, PROGRAM_BUFFER, temp_token);
+         token_type = ttok_type;
+      }
+      else
+      {
+         get_token(); /* get the next part of expression */
+         eval_exp2(answer);
+         vars[slot] = *answer;
+         return;
+      }
+   }
+
+   eval_exp2(answer);
 }
 
-void eval_exp2(double *answer)
+void eval_exp2(double* answer)
 {
+   register char operator; /* TODO: Introduce enum for operator types */
+   double temp;
 
+   eval_exp3(answer);
+
+   while ((operator = *token) == '+' || operator == '-')
+   {
+      get_token();
+      eval_exp3(&temp);
+
+      switch (operator) /* TOREFACTOR: Move it to function */
+      {
+      case '-':
+         *answer -= temp;
+         break;
+
+      case '+':
+         *answer += temp;
+         break;
+
+      default:
+         break;
+      }
+   }
 }
 
-void eval_exp3(double *answer)
+void eval_exp3(double* answer)
 {
+   register char operator; /* TODO: Introduce enum for operator types */
+   double temp;
 
+   eval_exp4(answer);
+
+   while ((operator = *token) == '*' || operator == '/' || operator == '%')
+   {
+      get_token();
+      eval_exp4(&temp);
+
+      switch (operator) /* TODO: Move it to function */
+      {
+      case '*':
+         *answer *= temp;
+         break;
+
+      case '/':
+         if (temp == 0.0)
+         {
+            /* TODO: Introduce enum for error types */
+            set_error(3); /* division by zero */
+            *answer = 0.0;
+         }
+         else
+         {
+            *answer /= temp;
+         }
+         break;
+
+      case '%':
+         *answer = (int)*answer % (int)temp;
+         break;
+
+      default:
+         break;
+      }
+   }
 }
 
-void eval_exp4(double *answer)
+void eval_exp4(double* answer)
 {
+   double temp, ex;
+   register int t;
 
+   eval_exp5(answer);
+   if (*token == '^')
+   {
+      get_token();
+      eval_exp4(&temp);
+      ex = *answer;
+      if (temp == 0.0)
+      {
+         *answer = 1.0;
+         return;
+      }
+
+      for (t = temp - 1; t > 0; --t)
+      {
+         *answer *= (double)ex;
+      }
+   }
 }
 
-void eval_exp5(double *answer)
+void eval_exp5(double* answer)
 {
+   register char operator; /* TODO: Introduce enum for operator types */
 
+   operator = 0;
+   if ((token_type == DELIMETER) && *token == '+' || *token == '-')
+   {
+      operator = *token;
+      get_token();
+   }
+
+   eval_exp6(answer);
+
+   if (operator == '-')
+   {
+      *answer = -*answer;
+   }
 }
 
-void eval_exp6(double *answer)
+void eval_exp6(double* answer)
 {
-
 }
 
-void atom(double *answer)
+void atom(double* answer)
 {
-
 }
 
 void get_token(void)
 {
-
 }
 
 void put_back(void)
 {
-
 }
 
 void set_error(int error)
 {
-
 }
 
-double find_var(char *s)
+double find_var(char* s)
 {
    return 0.0;
 }
--- Apps/Syntax parsing, Evaluation/Simple parser (improved)/eval_parser.h
+++ Apps/Syntax parsing, Evaluation/Simple parser (improved)/eval_parser.h
@@ -1,6 +1,7 @@
 #ifndef EVAL_PARSER_H
 #define EVAL_PARSER_H
 
+/* TODO: Introduce enum for type of tokens  */
 #define DELIMETER 1
 #define VARIABLE  2
 #define NUMBER    3
@@ -10,40 +11,57 @@
 /**
  * Points to the expression to be analyzed
  */
-extern char *program;
+extern char* program; /* TOREFACTOR: Encapsulate global/extern declarations */
 
 /**
  * 26 user variables, A-Z
  */
-extern double vars[];
+extern double vars[]; /* TOREFACTOR: Encapsulate global/extern declarations */
 
-char token[PROGRAM_BUFFER];
+char token[PROGRAM_BUFFER]; /* TOREFACTOR: Encapsulate global/extern declarations */
 
+/* TOREFACTOR: Encapsulate global/extern declarations */
 char token_type;
 
 /**
  * \brief Parser entry point
  * \param answer Recursive buffer value
  */
-void start_evaluation(double *answer);
+void start_evaluation(double* answer);
 
 /**
  * \brief Process an assignment
  * \param answer Recursively return expression value
  */
-void eval_exp1(double *answer);
+void eval_exp1(double* answer);
 
-void eval_exp2(double *answer);
+/**
+ * \brief Add or subtract two terms
+ * \param answer Recursively return expression value
+ */
+void eval_exp2(double* answer);
 
-void eval_exp3(double *answer);
+/**
+ * \brief Multiply or divide two factors
+ * \param answer Recursively return expression value
+ */
+void eval_exp3(double* answer);
 
-void eval_exp4(double *answer);
+/**
+ * \brief Process an exponent
+ * \param answer Recursively return expression value
+ */
+void eval_exp4(double* answer);
 
-void eval_exp5(double *answer);
+/**
+ * \brief Evaluate an unary + or -
+ * \param answer Recursively return expression value
+ */
+void eval_exp5(double* answer);
 
-void eval_exp6(double *answer);
+void eval_exp6(double* answer);
 
-void atom(double *answer);
+void atom(double* answer);
 
 void get_token(void);
 
@@ -51,7 +69,7 @@ void put_back(void);
 
 void set_error(int error);
 
-double find_var(char *s);
+double find_var(char* s);
 
 int is_delimeter(char symbol);
 
