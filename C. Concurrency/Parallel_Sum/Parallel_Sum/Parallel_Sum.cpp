144560de38b2c5b69cb055ed2e664216fcd0b903
SCHLEISSHEIMER\Vinevtsev <dwinner@inbox.ru>
2020-10-12 14:58:54 +0300

    Adding CMAke stub for parallel sum

---
+++ C. Concurrency/Parallel_Sum/Parallel_Sum/Parallel_Sum.cpp
@@ -0,0 +1,12 @@
+﻿// Parallel_Sum.cpp : Defines the entry point for the application.
+//
+
+#include "Parallel_Sum.h"
+
+using namespace std;
+
+int main()
+{
+   cout << "Hello CMake." << endl;
+   return 0;
+}
