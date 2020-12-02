986aec5151eea624b391637c588b5c53f8653200
SCHLEISSHEIMER\Vinevtsev <dwinner@inbox.ru>
2020-12-02 17:47:57 +0300

    Misc

--- C++. Networking/ipv6_tcp_blocking_socket/ipv6_tcp_server/ipv6_tcp_server.cpp
+++ C++. Networking/ipv6_tcp_blocking_socket/ipv6_tcp_server/ipv6_tcp_server.cpp
@@ -29,89 +29,90 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-   int listen_sock_fd = -1, client_sock_fd = -1;
-   struct sockaddr_in6 server_addr, client_addr;
-   socklen_t client_addr_len;
-   char str_addr[INET6_ADDRSTRLEN];
-   int ret, flag;   
+	int listen_sock_fd = -1, client_sock_fd = -1;
+	struct sockaddr_in6 server_addr, client_addr;
+	socklen_t client_addr_len;
+	char str_addr[INET6_ADDRSTRLEN];
+	int ret, flag;
 
 #ifdef _WIN32
-   WSADATA wsaData;
-   if (WSAStartup(MAKEWORD(2, 2), &wsaData))
-   {
-      perror("WSAStartup");
-      return 1;
-   }
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData))
+	{
+		perror("WSAStartup");
+		return 1;
+	}
 #endif
 
-   /* Create socket for listening (client requests) */
-   listen_sock_fd = socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
-   if (listen_sock_fd == -1)
-   {
-      perror("socket()");
-      return EXIT_FAILURE;
-   }
+	/* Create socket for listening (client requests) */
+	listen_sock_fd = socket(AF_INET6, SOCK_STREAM, IPPROTO_TCP);
+	if (listen_sock_fd == -1)
+	{
+		perror("socket()");
+		return EXIT_FAILURE;
+	}
 
-   /* Set socket to reuse address */
-   flag = 1;
-   ret = setsockopt(listen_sock_fd, SOL_SOCKET, SO_REUSEADDR, (const char*)&flag, sizeof(flag));
-   if (ret == -1)
-   {
-      perror("setsockopt()");
-      return EXIT_FAILURE;
-   }
+	/* Set socket to reuse address */
+	flag = 1;
+	ret = setsockopt(listen_sock_fd, SOL_SOCKET, SO_REUSEADDR, (const char*)&flag, sizeof(flag));
+	if (ret == -1)
+	{
+		perror("setsockopt()");
+		return EXIT_FAILURE;
+	}
 
-   memset(&server_addr, 0, sizeof(server_addr));
-   server_addr.sin6_family = AF_INET6;
-   server_addr.sin6_addr = in6addr_any;
-   server_addr.sin6_port = htons(SERVER_PORT);
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin6_family = AF_INET6;
+	server_addr.sin6_addr = in6addr_any;
+	server_addr.sin6_port = htons(SERVER_PORT);
 
-   /* Bind address and socket together */
-   ret = bind(listen_sock_fd, (struct sockaddr*)&server_addr, sizeof(server_addr));
-   if (ret == -1)
-   {
-      perror("bind()");
-      closesocket(listen_sock_fd);
-      return EXIT_FAILURE;
-   }
+	/* Bind address and socket together */
+	ret = bind(listen_sock_fd, reinterpret_cast<struct sockaddr*>(&server_addr), sizeof(server_addr));
+	if (ret == -1)
+	{
+		perror("bind()");
+		closesocket(listen_sock_fd);
+		return EXIT_FAILURE;
+	}
 
-   /* Create listening queue (client requests) */
-   ret = listen(listen_sock_fd, CLIENT_QUEUE_LEN);
-   if (ret == -1)
-   {
-      perror("listen()");
-      closesocket(listen_sock_fd);
-      return EXIT_FAILURE;
-   }
+	/* Create listening queue (client requests) */
+	ret = listen(listen_sock_fd, CLIENT_QUEUE_LEN);
+	if (ret == -1)
+	{
+		perror("listen()");
+		closesocket(listen_sock_fd);
+		return EXIT_FAILURE;
+	}
 
-   client_addr_len = sizeof(client_addr);
+	client_addr_len = sizeof(client_addr);
 
-   while (true)
-   {
-      /* Do TCP handshake with client */
-      client_sock_fd = accept(listen_sock_fd, (struct sockaddr*)&client_addr, &client_addr_len);
-      if (client_sock_fd == -1)
-      {
-         perror("accept()");
-         closesocket(listen_sock_fd);
-         return EXIT_FAILURE;
-      }
+	while (true)
+	{
+		/* Do TCP handshake with client */
+		client_sock_fd = accept(listen_sock_fd, reinterpret_cast<struct sockaddr*>(&client_addr), &client_addr_len);
+		if (client_sock_fd == -1)
+		{
+			perror("accept()");
+			closesocket(listen_sock_fd);
+			return EXIT_FAILURE;
+		}
 
-      inet_ntop(AF_INET6, &(client_addr.sin6_addr), str_addr, sizeof(str_addr));
-      printf("New connection from: %s:%d ...\n", str_addr, ntohs(client_addr.sin6_port));
+		inet_ntop(AF_INET6, &(client_addr.sin6_addr), str_addr, sizeof(str_addr));
+		printf("New connection from: %s:%d ...\n", str_addr, ntohs(client_addr.sin6_port));
 
-      /* Wait for data from client */
-      char buf[MSGBUFSIZE];
-      int addrlen = sizeof(client_addr);
-      int msgLen = recvfrom(client_sock_fd, buf, MSGBUFSIZE, 0, (struct sockaddr*)&client_addr, &addrlen);
-      if (msgLen == -1)
-      {
-         perror("recvfrom()");
-         closesocket(client_sock_fd);
-         continue;
-      }      
+		/* Wait for data from client */
+		char buf[MSGBUFSIZE];
+		int addrlen = sizeof(client_addr);
+		int msgLen = recvfrom(client_sock_fd, buf, MSGBUFSIZE, 0, reinterpret_cast<struct sockaddr*>(&client_addr),
+		                      &addrlen);
+		if (msgLen == -1)
+		{
+			perror("recvfrom()");
+			closesocket(client_sock_fd);
+			continue;
+		}
 
-      // Truncate the buffer
+		// Truncate the buffer
 		if (msgLen < MSGBUFSIZE)
 		{
 			int i = 0;
@@ -126,32 +127,33 @@ int _tmain(int argc, _TCHAR* argv[])
 			}
 		}
 
-      std::cout << "Client request " << buf; 
+		std::cout << "Client request " << buf;
 		std::string answer = mman::ManifestResponseMsg;
 
-      /* Send response to client */
-		msgLen = sendto(client_sock_fd, answer.c_str(), answer.length(), 0, (const struct sockaddr*)&client_addr, addrlen);
-      if (msgLen == -1)
-      {
-         perror("write()");
-         closesocket(client_sock_fd);
-         continue;
-      }
+		/* Send response to client */
+		msgLen = sendto(client_sock_fd, answer.c_str(), answer.length(), 0,
+		                reinterpret_cast<const struct sockaddr*>(&client_addr), addrlen);
+		if (msgLen == -1)
+		{
+			perror("write()");
+			closesocket(client_sock_fd);
+			continue;
+		}
 
-      /* Do TCP tear down */
-      ret = closesocket(client_sock_fd);
-      if (ret == -1)
-      {
-         perror("close()");
-         client_sock_fd = -1;
-      }
+		/* Do TCP tear down */
+		ret = closesocket(client_sock_fd);
+		if (ret == -1)
+		{
+			perror("close()");
+			client_sock_fd = -1;
+		}
 
-      printf("Connection closed\n");
-   }
+		printf("Connection closed\n");
+	}
 
 #ifdef _WIN32
-   WSACleanup();
+	WSACleanup();
 #endif
 
-   return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
