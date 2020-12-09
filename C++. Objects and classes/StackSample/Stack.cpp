00b23df2c1788ad45ec8a88579021a21627c86ef
SCHLEISSHEIMER\Vinevtsev <dwinner@inbox.ru>
2020-12-09 10:44:14 +0300

    C++ / object and classes

---
+++ C++. Objects and classes/StackSample/Stack.cpp
@@ -0,0 +1,41 @@
+﻿#include "Stack.h"
+
+namespace stacksample
+{
+   Stack::Stack() // create an empty stack
+   {
+      top_ = 0;
+   }
+
+   bool Stack::is_empty() const
+   {
+      return top_ == 0;
+   }
+
+   bool Stack::is_full() const
+   {
+      return top_ == max;
+   }
+
+   bool Stack::push(const Item& item)
+   {
+      if (top_ < max)
+      {
+         items_[top_++] = item;
+         return true;
+      }
+
+      return false;
+   }
+
+   bool Stack::pop(Item& item)
+   {
+      if (top_ > 0)
+      {
+         item = items_[--top_];
+         return true;
+      }
+
+      return false;
+   }
+}
---
+++ C++. Objects and classes/StockSample/Stock.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include <string>
+
+/**
+ * Stock class
+ */
+class Stock
+{
+public:
+   Stock();
+   Stock(const std::string& co, long n = 0, double pr = 0.0);
+   ~Stock();
+   void acquire(const std::string& co, long n, double pr);
+   void buy(long num, double price);
+   void sell(long num, double price);
+   void update(double price);
+   void show() const;
+   const Stock& top_val(const Stock& aStock) const;
+
+private:
+   std::string company_;
+   long shares_;
+   double shareVal_;
+   double totalVal_{};
+
+   void set_total()
+   {
+      totalVal_ = shares_ * shareVal_;
+   }
+};
