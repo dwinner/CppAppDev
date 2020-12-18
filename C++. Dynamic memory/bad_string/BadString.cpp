bf2cfbf441334a6714d5b141c623a9c2473aff98
SCHLEISSHEIMER\Vinevtsev <dwinner@inbox.ru>
2020-12-18 14:11:16 +0300

    Adaption VS To CMake

---
+++ C++. Dynamic memory/bad_string/BadString.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Vinevtsev on 12/18/2020.
+//
+
+#include "BadString.h"
---
+++ C++. Dynamic memory/bad_string/BadString.h
@@ -0,0 +1,8 @@
+//
+// Created by Vinevtsev on 12/18/2020.
+//
+
+#ifndef BAD_STRING_BADSTRING_H
+#define BAD_STRING_BADSTRING_H
+
+#endif //BAD_STRING_BADSTRING_H
