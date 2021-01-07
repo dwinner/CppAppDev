a7ebd6f1d62b00f912a4a5b6f17e05c555e82419
Денис Виневцев <dwinner@inbox.ru>
2021-01-07 15:23:24 +0300

    C++ 11+ / some samples

---
+++ C++. STD-11+/callable_templates/main.cpp
@@ -0,0 +1,40 @@
+/**
+ * Callable types and templates - wrong example
+ * (too many templates were created there)
+ */
+
+#include <iostream>
+#include <math.h>
+#include "somedefs.h"
+
+using namespace std;
+
+double dub(double x)
+{ return 2.0 * x; }
+
+int main()
+{
+   double y = 1.21;
+
+   cout << "Function pointer dub:\n";
+   cout << "  " << use_f(y, dub) << endl;
+
+   // cout << "Function pointer sqrt:\n";
+   // cout << "  " << use_f(y, sqrt) << endl;
+
+   cout << "Function object Fp:\n";
+   cout << "  " << use_f(y, Fp(5.0)) << endl;
+
+   cout << "Function object Fq:\n";
+   cout << "  " << use_f(y, Fq(5.0)) << endl;
+
+   cout << "Lambda expression 1:\n";
+   cout << "  " << use_f(y, [](double u)
+   { return u * u; }) << endl;
+
+   cout << "Lambda expresson 2:\n";
+   cout << "  " << use_f(y, [](double u)
+   { return u + u / 2.0; }) << endl;
+
+   return 0;
+}
