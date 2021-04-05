f591b551522b5e244d8f340aeaf632567c82c238
Denis Vinevcev <dwinner@inbox.ru>
2021-04-05 22:34:19 +0300

    GCC touch

---
+++ appDev. C/C. Compiling with GCC/optimization/nopt-sample/cmake-build-release/CMakeFiles/nopt_sample.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nopt_sample.dir/eval_parser.c.obj"
+  "CMakeFiles/nopt_sample.dir/main.c.obj"
+  "nopt_sample.exe"
+  "nopt_sample.exe.manifest"
+  "nopt_sample.lib"
+  "nopt_sample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/nopt_sample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
