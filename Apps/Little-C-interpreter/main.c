dc288636bf1fce3fbf89917f00777ecc5f1cbc02
SCHLEISSHEIMER\Vinevtsev <dwinner@inbox.ru>
2021-08-27 11:16:30 +0300

    some org changes

---
+++ Apps/Little-C-interpreter/main.c
@@ -0,0 +1,11 @@
+/**
+ * Recursive decent parser for Little-C interpreter
+ */
+
+#include <stdio.h>
+
+int main()
+{
+   printf("Hello, World!\n");
+   return 0;
+}
--- apps/Little-C-interpreter/LittleC/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-   printf("start");
-
-   return 0;
-}
