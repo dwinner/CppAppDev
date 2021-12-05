e5cfc6f83bdb1cab7b1bb04c7ed4a1b69cd46bcf
vinevtsev <vinevtsev@schleissheimer.de>
2021-12-05 17:39:11 +0100

    shell execution sample - wip

--- appDev. Windows/win32Api/ShellExecuteSample/ShellExecuteSample.cpp
+++ appDev. Windows/win32Api/ShellExecuteSample/ShellExecuteSample.cpp
@@ -1,13 +1,27 @@
+/**
+ * Installing / managing VS-code extensions from C++
+ */
+
 #include "pch.h"
 #include "framework.h"
 #include "ShellExecuteSample.h"
+
+#include <cassert>
+
+#include "UserEnv.h"
 #include <filesystem>
+#include <string>
+#include <set>
+#include <sstream>
+
+#pragma comment(lib, "userenv.lib")
 
 using namespace std;
+namespace fs = std::filesystem;
 
-int main()
+int main() noexcept
 {
-   auto currentExeDir = std::filesystem::current_path();
+   //auto currentExeDir = std::filesystem::current_path();
 
    HMODULE hModule = ::GetModuleHandle(nullptr);
    if (hModule == nullptr)
@@ -23,22 +37,60 @@ int main()
       return EXIT_FAILURE;
    }
 
-   /*
-    * --install-extension caneasy-1.0.0.vsix
-    * --uninstall-extension kmasif.capl-vector
-    * "c:\Users\vinevtsev\AppData\Local\Programs\Microsoft VS Code\bin\code.cmd" --install-extension "d:\CanEasy\Multidebugger\Debug\caneasy-1.0.0.vsix"
-    */
-   auto operation = L"open";
-   auto commandFile = L"cmd";
-   auto commandParams =
-      L"/c \"c:\\Users\\vinevtsev\\AppData\\Local\\Programs\\Microsoft VS Code\\bin\\code.cmd\" --install-extension caneasy-1.0.0.vsix";
-   auto currentDir = nullptr;
-   HINSTANCE retVal = ShellExecute(nullptr,
-                                   operation,
-                                   commandFile,
-                                   commandParams,
-                                   currentDir,
-                                   SW_HIDE);
+   // Check extensions have been already installed
+   string userDir = getUserHomeDir();
+   auto extensionsDir = fs::path(userDir);
+   extensionsDir.concat("\\.vscode\\extensions");
+   assert(fs::exists(extensionsDir));
+
+   // Get all installed extensions
+   string ceVsixPath = "caneasy-1.0.0.vsix";
+   string caplPath = "kmasif.capl-vector";
+   set<fs::path> extensionSet;
+   for (const auto& directoryEntry : fs::directory_iterator(extensionsDir))
+   {
+      extensionSet.insert(directoryEntry.path());
+   }
+
+   string codeCmdPath = R"(c:\Users\vinevtsev\AppData\Local\Programs\Microsoft VS Code\bin\code.cmd)";
+   assert(fs::exists(codeCmdPath));
+   assert(fs::exists(ceVsixPath));
+   installExtension(codeCmdPath, ceVsixPath);
+   installExtension(codeCmdPath, caplPath, false);
 
    return EXIT_SUCCESS;
+}
+
+std::string getUserHomeDir()
+{
+   TCHAR szHomeDirBuf[MAX_PATH] = {0};
+   HANDLE hToken = nullptr;
+   VERIFY(OpenProcessToken( GetCurrentProcess(), TOKEN_QUERY, &hToken ));
+   DWORD bufSize = MAX_PATH;
+   VERIFY(GetUserProfileDirectory( hToken, szHomeDirBuf, &bufSize ));
+   CloseHandle(hToken);
+   std::string homeDir = szHomeDirBuf;
+
+   return homeDir;
+}
+
+bool installExtension(const std::string& vsCodeCmd, const std::string& extensionPath, const bool install)
+{
+   constexpr auto operation = "open";
+   constexpr auto shell = "cmd";
+   const std::string cmdOpt = "/c";
+   const std::string vsCodeCommand = install ? "--install-extension" : "--uninstall-extension";
+   std::ostringstream fmtStream;
+   fmtStream << cmdOpt << " \"" << vsCodeCmd << "\" " << vsCodeCommand << " " << extensionPath;
+   const std::string shellLaunchArgs = fmtStream.str();
+   const auto cmdParams = shellLaunchArgs.c_str();
+   HINSTANCE retVal = ShellExecute(nullptr,
+                                   operation,
+                                   shell,
+                                   cmdParams,
+                                   nullptr, // used current execution directory due to shell call issues in Win32 API
+                                   SW_HIDE);
+   const bool success = retVal > nullptr ? true : false;
+
+   return success;
 }
--- appDev. Windows/win32Api/ShellExecuteSample/ShellExecuteSample.h
+++ appDev. Windows/win32Api/ShellExecuteSample/ShellExecuteSample.h
@@ -1,3 +1,11 @@
 #pragma once
 
+#include <sstream>
+
 #include "resource.h"
+#include <string>
+#include <UserEnv.h>
+
+inline std::string getUserHomeDir();
+
+inline bool installExtension(const std::string& vsCodeCmd, const std::string& extensionPath, bool install = true);
