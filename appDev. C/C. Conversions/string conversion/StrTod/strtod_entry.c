7f0645c3d2d4b2c1e2b48f04e5aa7205d57d7b95
vinevtsev <vinevtsev@schleissheimer.de>
2022-03-28 13:33:09 +0200

    Misc

--- appDev. C/C. Conversions/string conversion/StrTod/strtod_entry.c
+++ appDev. C/C. Conversions/string conversion/StrTod/strtod_entry.c
@@ -1,15 +1,17 @@
-// Fig. 8.6: fig08_06.c
-// Using function strtod
+/**
+ * Using function strtod
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void)
-{ 
-   const char *string = "51.2% are admitted"; // initialize string
-   char *stringPtr; // create char pointer
-   
-   double d = strtod(string, &stringPtr);
-   
-   printf("The string \"%s\" is converted to the\n", string);
-   printf("double value %.2f and the string \"%s\"\n", d, stringPtr);
+{
+    const char *string = "51.2% are admitted"; // initialize string
+    char *stringPtr; // create char pointer
+
+    double d = strtod(string, &stringPtr);
+
+    printf("The string \"%s\" is converted to the\n", string);
+    printf("double value %.2f and the string \"%s\"\n", d, stringPtr);
 } 
--- appDev. C/C. Conversions/string conversion/StrTol/strtol_entry.c
+++ appDev. C/C. Conversions/string conversion/StrTol/strtol_entry.c
@@ -1,19 +1,20 @@
-// Fig. 8.7: fig08_07.c
-// Using function strtol
+/**
+ * Using function strtol
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void)
-{                  
-   const char *string = "-1234567abc"; // initialize string pointer  
-   char *remainderPtr; // create char pointer
-   
-   long x = strtol(string, &remainderPtr, 0);
+{
+    const char *string = "-1234567abc"; // initialize string pointer
+    char *remainderPtr; // create char pointer
+    long x = strtol(string, &remainderPtr, 0);
 
-   printf("%s\"%s\"\n%s%ld\n%s\"%s\"\n%s%ld\n",
-      "The original string is ", string,
-      "The converted value is ", x,
-      "The remainder of the original string is ", 
-      remainderPtr,
-      "The converted value plus 567 is ", x + 567);
+    printf("%s\"%s\"\n%s%ld\n%s\"%s\"\n%s%ld\n",
+           "The original string is ", string,
+           "The converted value is ", x,
+           "The remainder of the original string is ",
+           remainderPtr,
+           "The converted value plus 567 is ", x + 567);
 } 
