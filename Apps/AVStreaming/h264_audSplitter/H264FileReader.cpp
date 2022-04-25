2e49b69ed663af55c490319bfa9249237d6a29a2
Denis Vinevcev <dwinner@inbox.ru>
2022-04-25 22:22:30 +0300

    Fix checking AUD

--- Apps/AVStreaming/h264_audSplitter/H264FileReader.cpp
+++ Apps/AVStreaming/h264_audSplitter/H264FileReader.cpp
@@ -2,48 +2,66 @@
 #include <vector>
 #include <fstream>
 #include <regex>
+#include <cassert>
 
 H264FileReader::~H264FileReader() = default;
 
 H264FileReader::H264FileReader(const std::string &h264FileName)
 {
-    h264FileName_ = h264FileName;
+   h264FileName_ = h264FileName;
 }
 
 std::unique_ptr<std::valarray<std::unique_ptr<Nalu>>> H264FileReader::GetNalUnits() const
 {
-    using namespace std;
+   using namespace std;
 
-    ifstream h264FileStream(h264FileName_, ios::binary);
-    string h264Content((std::istreambuf_iterator<char>(h264FileStream)),
-                       (std::istreambuf_iterator<char>()));
-    vector<string> splitByAudList = SplitByRegex(h264Content, audNaluPattern);
-    size_t nalUnitsCount = splitByAudList.size();
-    auto naluValues = make_unique<valarray<unique_ptr<Nalu>>>(nalUnitsCount - 1);
+   ifstream h264FileStream(h264FileName_, ios::binary);
+   string h264Content((std::istreambuf_iterator<char>(h264FileStream)),
+                      (std::istreambuf_iterator<char>()));
+   vector<string> splitByAudList = SplitByRegex(h264Content, audNaluPattern);
+   size_t nalUnitsCount = splitByAudList.size();
 
-    // iterate through the NAL-units, skip the 1st one - it's always empty
-    for (size_t naluIdx = 1; naluIdx < nalUnitsCount; naluIdx++)
-    {
-        string strContent = splitByAudList[naluIdx];
-        const size_t length = strContent.length();
-        valarray<uint8_t> content = valarray<uint8_t>(length);
-        for (size_t strIdx = 0; strIdx < length; strIdx++)
-        {
-            content[strIdx] = static_cast<uint8_t>(strContent[strIdx]);
-        }
+   // Check parity before gathering frames
+   assert(((nalUnitsCount - 1) % 2) == 0);
 
-        (*naluValues)[naluIdx - 1] = make_unique<Nalu>(length, content);
-    }
+   auto naluValues = make_unique<valarray<unique_ptr<Nalu>>>((nalUnitsCount - 1) / 2);
 
-    return naluValues;
+   // iterate through the NAL-units, skip the 1st one - it's always empty
+   size_t chunkIndex = 0;
+   for (size_t naluIdx = 1; naluIdx < nalUnitsCount; naluIdx += 2)
+   {
+      string firstChunk = splitByAudList[naluIdx];
+      const size_t firstChunkLen = firstChunk.length();
+
+      string secondChunk = splitByAudList[naluIdx + 1];
+      const size_t secondChunkLen = secondChunk.length();
+
+      const size_t totalFrameLen = firstChunkLen + secondChunkLen;
+
+      valarray<uint8_t> content = valarray<uint8_t>(totalFrameLen);
+      size_t strIdx;
+      for (strIdx = 0; strIdx < firstChunkLen; strIdx++)
+      {
+         content[strIdx] = static_cast<uint8_t>(firstChunk[strIdx]);
+      }
+
+      for (strIdx = 0; strIdx < secondChunkLen; strIdx++)
+      {
+         content[strIdx + firstChunkLen] = static_cast<uint8_t>(secondChunk[strIdx]);
+      }
+
+      (*naluValues)[chunkIndex++] = make_unique<Nalu>(totalFrameLen, content);
+   }
+
+   return naluValues;
 }
 
 std::vector<std::string> H264FileReader::SplitByRegex(const std::string &inputString, const std::string &regEx)
 {
-    using namespace std;
+   using namespace std;
 
-    static regex reInstance{regEx};
-    sregex_token_iterator start{inputString.begin(), inputString.end(), reInstance, -1}, end;
+   static regex reInstance{regEx};
+   sregex_token_iterator start{inputString.begin(), inputString.end(), reInstance, {-1, 0}}, end;
 
-    return vector<string>(start, end);
+   return vector<string>(start, end);
 }
--- Apps/AVStreaming/h264_audSplitter/main.cpp
+++ Apps/AVStreaming/h264_audSplitter/main.cpp
@@ -5,53 +5,57 @@
 
 int main()
 {
-    const auto h264TestFile = "test_noloop.h264";
+   const auto h264TestFile = "test_noloop.h264";
 
-    using namespace std;
+   using namespace std;
 
-    unique_ptr<H264FileReader> h264FileReader = make_unique<H264FileReader>(h264TestFile);
-    unique_ptr<valarray<unique_ptr<Nalu>>> nalUnits = h264FileReader->GetNalUnits();
-    size_t nalUnitsCount = nalUnits->size();
+   unique_ptr<H264FileReader> h264FileReader = make_unique<H264FileReader>(h264TestFile);
+   unique_ptr<valarray<unique_ptr<Nalu>>> nalUnits = h264FileReader->GetNalUnits();
+   size_t nalUnitsCount = nalUnits->size();
 
-    assert(nalUnitsCount > 0);
+   assert(nalUnitsCount > 0);
 
-    // check the expected NALU count
-    const size_t expectedNaluCount = 145;
-    assert(nalUnitsCount == expectedNaluCount);
+   // check the expected NALU count
+   const size_t expectedNaluCount = 145;
+   assert(nalUnitsCount == expectedNaluCount);
 
-    for (size_t nalIdx = 0; nalIdx < nalUnitsCount; nalIdx++)
-    {
-        unique_ptr<Nalu> naluItem = move((*nalUnits)[nalIdx]);
-        size_t length = naluItem->GetLength();
-        std::valarray<uint8_t> content = naluItem->GetContent();
+   for (size_t nalIdx = 0; nalIdx < nalUnitsCount; nalIdx++)
+   {
+      unique_ptr<Nalu> naluItem = move((*nalUnits)[nalIdx]);
+      size_t length = naluItem->GetLength();
+      std::valarray<uint8_t> content = naluItem->GetContent();
 
-        // Check that AUD has 00 00 00 01 in the 1st 4 elements
-        uint8_t byte1 = content[0];
-        uint8_t byte2 = content[1];
-        uint8_t byte3 = content[2];
-        uint8_t byte4 = content[3];
+      // Check that marker has 00 00 00 01 in the 1st 4 elements
+      uint8_t byte1 = content[0];
+      uint8_t byte2 = content[1];
+      uint8_t byte3 = content[2];
+      uint8_t byte4 = content[3];
 
-        const auto zeroByte = static_cast<uint8_t>(0);
-        const auto oneByte = static_cast<uint8_t>(1);
+      const auto zeroByte = static_cast<uint8_t>(0);
+      const auto oneByte = static_cast<uint8_t>(1);
 
-        assert(byte1 == zeroByte);
-        assert(byte2 == zeroByte);
-        assert(byte3 == zeroByte);
-        assert(byte4 == oneByte);
+      assert(byte1 == zeroByte);
+      assert(byte2 == zeroByte);
+      assert(byte3 == zeroByte);
+      assert(byte4 == oneByte);
 
-        cout << "Length: " << length << endl;
-        cout << "Content: " << endl;
-        for (size_t i = 0; i < length; i++)
-        {
-            printf(" %02x", content[i]);
-            if ((i + 1) % 16 == 0)
-            {
-                cout << endl;
-            }
-        }
+      // Check that AUD has 09 at the 5th byte
+      uint8_t byte5 = content[4];
+      assert(byte5 == static_cast<uint8_t>(9));
 
-        cout << endl << endl;
-    }
+      cout << "Length: " << length << endl;
+      cout << "Content: " << endl;
+      for (size_t i = 0; i < length; i++)
+      {
+         printf(" %02x", content[i]);
+         if ((i + 1) % 16 == 0)
+         {
+            cout << endl;
+         }
+      }
 
-    return 0;
+      cout << endl << endl;
+   }
+
+   return 0;
 }
