98fdeb7fd34352a0897bb50eb5c0ca67fbc2b545
Denis Vinevcev <dwinner@inbox.ru>
2024-06-24 17:54:04 +0300

    tried out the Rust PL env.

---
+++ appDev. Rust/intro/cereals/src/main.rs
@@ -0,0 +1,17 @@
+#[derive(Debug)]
+enum Cereal {
+    Barley,
+    Miller,
+    Rice,
+    Rye,
+    Spelt,
+    Wheat,
+}
+
+fn main() {
+    let mut grains: Vec<Cereal> = vec![];
+    grains.push(Cereal::Rye);
+    drop(grains);
+
+    println!("{:?}", grains);
+}
