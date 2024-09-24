5e0aefa566d7b3c11e90dff12d1ceb5dbbccd36a
vinevtsev <vinevtsev@schleissheimer.de>
2024-09-24 18:55:18 +0200

    Ex for classes

---
+++ cpp/+ex/classes/personSample/Person.cpp
@@ -0,0 +1,80 @@
+#include "Person.hpp"
+
+#include <utility>
+
+Person::Person(const std::string &firstName, const std::string &lastName)
+    : Person{firstName, lastName, extractInitials(firstName, lastName)}
+{
+}
+
+Person::Person() : Person{"", "", ""}
+{
+}
+
+Person::Person(const Person &src) = default;
+
+Person &Person::operator=(const Person &rhs) = default;
+
+inline const std::string &Person::getFirstName() const
+{
+   return firstName_;
+}
+
+void Person::setFirstName(const std::string &firstName)
+{
+   firstName_ = firstName;
+}
+
+inline const std::string &Person::getLastName() const
+{
+   return lastName_;
+}
+
+void Person::setLastName(const std::string &lastName)
+{
+   lastName_ = lastName;
+}
+
+const std::string &Person::getInitials() const
+{
+   return initials_;
+}
+
+void Person::setInitials(const std::string &initials)
+{
+   initials_ = initials;
+}
+
+Person::Person(std::string firstName, std::string lastName, std::string initials)
+    : firstName_(std::move(firstName)),
+      lastName_(std::move(lastName)),
+      initials_(std::move(initials))
+{
+}
+
+std::string Person::extractInitials(const std::string &firstName, const std::string &lastName)
+{
+   std::string initials;
+   if (!firstName.empty())
+   {
+      initials += static_cast<char>(std::toupper(firstName[0]));
+   }
+
+   if (!lastName.empty())
+   {
+      initials += static_cast<char>(std::toupper(lastName[0]));
+   }
+
+   return initials;
+}
+
+Person::~Person() = default;
+
+std::ostream &operator<<(std::ostream &ostream, const Person &person)
+{
+   ostream <<
+           "First name: " << person.getFirstName() <<
+           ", Last name: " << person.getLastName() <<
+           ", Initials: " << person.getInitials();
+   return ostream;
+}
---
+++ cpp/+ex/classes/personSample/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <array>
+#include "Person.hpp"
+
+static constexpr const auto empLen = 3;
+using namespace std;
+
+int main()
+{
+   Person person1{"Sata", "Jones"};
+   cout << person1 << endl;
+
+   auto person2 = make_unique<Person>("Holy", "Water");
+   cout << *person2 << endl;
+
+   array<Person, empLen> employees;
+   for (auto &employee: employees)
+   {
+      employee = person1;
+   }
+
+   employees[0].setFirstName("Den");
+   employees[0].setLastName("Win");
+
+   for (const auto &person: employees)
+   {
+      cout << person << endl;
+   }
+
+   return 0;
+}
