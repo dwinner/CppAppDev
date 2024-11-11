9a45e1fd5e1f7a38736c6de21581148e97aee8a1
vinevtsev <vinevtsev@schleissheimer.de>
2024-11-11 17:10:12 +0100

    c++23 - qa

---
+++ cpp/qa/objectPoolTest/objectPoolTest.hpp
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "pch.h"
+#include <CppUnitTest.h>
+
+namespace objectPoolTest
+{
+   TEST_CLASS(ObjectPoolTest)
+   {
+   public:
+      TEST_CLASS_INITIALIZE(setUp);
+
+      TEST_CLASS_CLEANUP(tearDown);
+
+      TEST_METHOD(testSimple);
+
+      TEST_METHOD(testException);
+
+      TEST_METHOD(testCreation);
+
+      TEST_METHOD(testAcquire);
+
+      TEST_METHOD(testExclusivity);
+
+      TEST_METHOD(testRelease);
+   };
+}
