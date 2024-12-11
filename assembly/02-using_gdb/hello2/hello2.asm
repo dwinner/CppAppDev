0a20af4b83e166cfcc2f533a034bbc561b4f7d43
Denis Vinevcev <dwinner@inbox.ru>
2024-12-11 22:03:21 +0300

    asm. formatted

--- assembly/02-using_gdb/hello2/hello2.asm
+++ assembly/02-using_gdb/hello2/hello2.asm
@@ -1,23 +1,23 @@
 ; hello2.asm
 section .data
-    msg db      "hello, world",0
-    NL  db      0xa                 ; ascii code for new line
+   msg db      "hello, world",0
+   NL  db      0xa                 ; ascii code for new line
 section .bss
 section .text
-    global main
+   global main
 main:
 
-    mov rax, 1      ; 1 = write
-    mov rdi, 1      ; 1 = to stdout
-    mov rsi, msg    ; string to display
-    mov rdx, 12     ; length of the string, without 0
-    syscall         ; display the string
-    mov rax, 1      ; 1 = write
-    mov rdi, 1      ; 1 = to stdout
-    mov rsi, NL     ; display the new line
-    mov rdx, 1      ; length of the string
-    syscall         ; display the string
+   mov   rax, 1      ; 1 = write
+   mov   rdi, 1      ; 1 = to stdout
+   mov   rsi, msg    ; string to display
+   mov   rdx, 12     ; length of the string, without 0
+   syscall         ; display the string
+   mov   rax, 1      ; 1 = write
+   mov   rdi, 1      ; 1 = to stdout
+   mov   rsi, NL     ; display the new line
+   mov   rdx, 1      ; length of the string
+   syscall         ; display the string
 
-    mov rax, 60     ; 60 = exit
-    mov rdi, 0      ; 0 = success exit code
-    syscall         ; quit
+   mov   rax, 60     ; 60 = exit
+   mov   rdi, 0      ; 0 = success exit code
+   syscall         ; quit
